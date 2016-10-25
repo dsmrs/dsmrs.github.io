b5ccc27a2ec4f88b8d263ca7d881f5eb716792ce
Dorian Somers <doriansomers@gmail.com>
2016-10-25 15:30:38 +0200

    add animatation

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
-
+// This is ulgy but it works! :-)
 $( document ).ready(function() {
-  $('[data-toggle="tooltip"]').tooltip()
-  var box = $('#box');
+  $('[data-toggle="tooltip"]').tooltip();
 
-  $('#hiking').on('click', function (e) {
-    e.preventDefault();
-    box.toggleClass('hidden');
-    box.toggleClass('visuallyhidden');
-    box.toggleClass('box');
-  });
+  var currentElement = null;
+  var buttonsToggle = $('.toggle')
+
+  function hide(element) {
+    var elementToHide = $(element)
+    elementToHide.animate({
+      height: "0px",
+      opacity: "0"
+    }, 1000);
+    setTimeout(function () {
+      elementToHide.hide();
+    }, 1000);
+  }
+
+  function show(element) {
+    var elementToShow = $(element)
+    elementToShow.show();
+    elementToShow.animate({
+      height: "300px",
+      opacity: "1"
+    }, 1000);
+  }
+
+  function toggleContent(element) {
+    hide(currentElement);
+    if ($(element).attr('id') !== $(currentElement).attr('id')) {
+      currentElement = element;
+      setTimeout(function () {
+        show(element);
+      }, 1000);
+    }
+  }
+  var ids = {}
+
+  for (var i = 0; i < buttonsToggle.length; i++) {
+    $(buttonsToggle[i]).on('click', function (e) {
+      this.id = "#" + $(e.target).attr("toggle");
+      this.newElement = $(this.id)
+      if (this.newElement) {
+        toggleContent(this.newElement);
+      }
+    });
+  }
 });
